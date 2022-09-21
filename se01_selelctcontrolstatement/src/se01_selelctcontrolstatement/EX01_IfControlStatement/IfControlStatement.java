f21defc7ac0cf1bdb1fe4016126692c8b72b208d
ko9un <ktw1633@gmail.com>
2022-09-21 16:14:46 +0900

    9월21일 오후수업 3가지 유형의 IF문

---
+++ se01_selelctcontrolstatement/src/se01_selelctcontrolstatement/EX01_IfControlStatement/IfControlStatement.java
@@ -0,0 +1,80 @@
+package se01_selelctcontrolstatement.EX01_IfControlStatement;
+
+// 116p 3가지 유형의 if 문 
+
+public class IfControlStatement {
+	public static void main(String[] args) {
+		// 유형 : if
+		int value1 = 5;
+		if (value1 > 3) {
+			System.out.println("실행1");
+		}
+		if (value1 < 5) {
+			System.out.println("실행2");
+		}
+
+		boolean bool1 = true;
+		boolean bool2 = false;
+		if (bool1) {
+			System.out.println("실행3");
+		}
+		if (bool2) {
+			System.out.println("실행4");
+		}
+
+		// 유형 : if-else
+		int value2 = 5;
+		if (value2 > 3) {
+			System.out.println("실행5");
+		} else {
+			System.out.println("실행6");
+		}
+
+		// cf. 삼항 연산자와 변환 가능
+		System.out.println((value2 > 3) ? "실행5" : "실행6");
+		System.out.println();
+
+		// 유형 3 : if-else if-else if-...-else
+		int value3 = 85;
+		if(value3 >=90 ) {
+			System.out.println("A학점");
+		}
+		else if(value3 >=80 ) {
+			System.out.println("B학점");
+		}
+		else if(value3 >=70 ) {
+			System.out.println("C학점");
+		}
+		else {
+			System.out.println("F학점");
+		}
+		
+		if(value3 >=70 ) {
+			System.out.println("C학점");
+		}
+		else if(value3 >= 80) {
+			System.out.println("B학점");
+		}
+		else {
+			System.out.println("F학점");
+		}
+	
+	
+	if(value3 >= 70 && value3 < 80) {
+		System.out.println("C학점");
+	}
+	else if(value3 >= 80 && value3 < 90) {
+		System.out.println("B학점");
+	}
+	else if(value3 >= 90) {
+		System.out.println("A학점");
+	}
+	else {
+		System.out.println("F학점");
+	}
+	
+	
+	}
+	
+
+}
